9bd1080ffd671b5f115d6c7678914f3364700cc0
Ксения Че <chechulina75@list.ru>
2024-07-22 20:58:40 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,20 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len(my_list):
+    if my_list[a] > 0:
+        print(my_list[a])
+        a = a + 1
+    elif my_list[a] == 0:
+        a = a + 1
+        continue
+    else:
+        break
+
+
+
+
+
+
+
+
+
